4d7ecbdb00c5b2edcc4b00bf8b3b1f6789542319
wwhitlow <wwhitlow@gatech.edu>
2017-11-15 10:43:58 -0500

    Created a short little python script for hitting API to start recording forecast data for future training. Filename based on date and creates a list of the file names for future parsing.

---
+++ dataCollector.py
@@ -0,0 +1,21 @@
+import datetime
+import requests
+import json
+import os
+
+API_TOKEN = os.environ["API_TOKEN"]
+
+url = 'http://api.wunderground.com/api/'+ API_TOKEN + '/forecast/q/GA/Atlanta.json'
+headers = {'Accept': 'application/json'}
+response = requests.get(url, headers=headers)
+
+
+filename = str(datetime.datetime.today().strftime('%b-%d-%Y:%H')) + '.json'
+
+with open('collectedFile.txt', 'a') as dataList:
+    dataList.write(filename)
+    dataList.write('\n')
+
+with open('collectedData/'+filename, 'w') as outf:
+    outf.write(response.content)
+
